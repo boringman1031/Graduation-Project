eeb5e6f59d04d24ab3fdb8d31ca7de228b587c12
boringman <ian.lin1031@gmail.com>
2024-10-21 22:42:55 +0800

    [update] 10/21角色攻擊、移動進度
    包含場景初步架設
    
    Signed-off-by: boringman <ian.lin1031@gmail.com>

--- Grduation_Game/Assets/Script/PlayerBase.cs
+++ Grduation_Game/Assets/Script/PlayerBase.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 using UnityEngine.InputSystem   ;
 
 public class PlayerBase : MonoBehaviour,GameData
-{   
+{
+    [SerializeField]
+    private GameObject Player_front;
+    [SerializeField]
+    private GameObject Player_side;
+
     Rigidbody2D Player_rb;
+    Animator Player_animator;
     private float InputX,InputY;
     private bool isFlip = false;
+    private int AttackState = 0;
     public int Health { get; set; }
     public int Defence { get; set; }
     public float Speed { get; set; }
@@ -17,34 +24,76 @@ public class PlayerBase : MonoBehaviour,GameData
     void Awake()
     {
         Player_rb = GetComponent<Rigidbody2D>();
+        Player_animator = GetComponent<Animator>();
+        Speed = 5.0f;
     }
 
     void Update()
     {
-        Player_rb.velocity = new Vector2( Speed*InputX, Speed*InputY);
-    }   
+        Player_rb.velocity = new Vector2(Speed * InputX, Player_rb.velocity.y);
+        if (Player_rb.velocity.magnitude == 0)
+        {
+            Player_animator.SetBool("isRun", false);
+            Player_front.SetActive(true);
+            Player_side.SetActive(false);
+        }
+    }
     public void PlayerMove(InputAction.CallbackContext context)
     {
-        Debug.Log(context);
+        Player_front.SetActive(false);
+        Player_side.SetActive(true);
+        Player_animator.SetBool("isRun", true);
         InputX = context.ReadValue<Vector2>().x;
-        InputY = context.ReadValue<Vector2>().y;
-        if((Player_rb.velocity.x  < 0 && !isFlip) || (Player_rb.velocity.x > 0 && isFlip))
+        if ((Player_rb.velocity.x < 0 && !isFlip) || (Player_rb.velocity.x > 0 && isFlip))
         {
             isFlip = !isFlip;
             transform.Rotate(0, 180, 0);
         }
     }
-    public void PlayerAttack()
+    public void PlayerAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Player Attack");
+        if (context.performed)
+        {
+            switch (AttackState)
+            {
+                case 0:
+                    Player_animator.SetTrigger("Attack1");
+                    AttackState += 1;
+                    break;
+                case 1:
+                    Player_animator.SetTrigger("Attack2");
+                    AttackState += 1;
+                    break;
+                case 2:
+                    Player_animator.SetTrigger("Attack3");
+                    AttackState = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+    public void PlayerSkill1(InputAction.CallbackContext context)
+    {
+        Debug.Log("Player Connected");
+
+    }
+    public void PlayerSkill2(InputAction.CallbackContext context)
+    {
+        Debug.Log("Player Skill2");
+    }
+    public void PlayerSkill3(InputAction.CallbackContext context)
+    {
+        Debug.Log("Player Skill3");
+    }
+
 
     private void PlayerHit(int _damage)
     {
         Health -= _damage;
         
     }
-    public void PlayerDie()
+    private  void PlayerDie()
     {
         Debug.Log("Player Die");
     }       
