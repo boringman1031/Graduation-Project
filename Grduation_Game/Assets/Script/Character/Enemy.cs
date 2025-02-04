64e07f7f2f506b027a19c759658a02f8f5ef6efb
Boringman1031 <ian.lin1031@gmail.com>
2025-02-05 02:39:37 +0800

    [Fix] 修正Zenject插件未使用造成的讀檔問題

---
+++ Grduation_Game/Assets/Script/Character/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("°òÂ¦¼Æ­È")]
+    public float normalSpeed;
+    public float chaseSpeed;
+    public float currentSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
