427f77a73152e2c1d05adc43f58da778bd5a8ec0
Boringman1031 <ian.lin1031@gmail.com>
2025-02-26 00:37:16 +0800

    [Fix] 修正轉場動畫無法顯示問題

--- Grduation_Game/Assets/Script/UI/SceneLoader.cs
+++ Grduation_Game/Assets/Script/UI/SceneLoader.cs
@@ -8,13 +8,13 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour,ISaveable
+public class SceneLoader : MonoBehaviour, ISaveable
 {
     [Header("廣播")]
     public VoidEventSO saveDataEvent;//儲存加載遊戲事件
     public VoidEventSO afterSceneLoadedEvent;
     public VoidEventSO openRandomCanvaEvent;//打開隨機挑戰面板
-    public SceneLoadEventSO unLoadSceneEvent;
+    public SceneLoadEventSO unLoadSceneEvent;//卸載場景事件
     public TransitionEventSO transitionEvent;
 
     [Header("事件監聽")]
@@ -22,14 +22,14 @@ public class SceneLoader : MonoBehaviour,ISaveable
     public VoidEventSO onAllEnemiesDefeated;//當場景中所有敵人被擊敗時的事件
     public VoidEventSO loadRandomSceneEvent;//隨機場景加載事件
     public VoidEventSO newGameEvent;
-    public VoidEventSO backToMenuEvent; 
+    public VoidEventSO backToMenuEvent;
 
     [Header("場景參數")]
     public GameSceneSO firstLoadScene;//第一個加載的場景(遊戲大聽)
     private GameSceneSO sceneToLoad;//要新遊戲開始要加載的場景
     public GameSceneSO MuneScene;//主場景
     private GameSceneSO currentLoadScene;//當前加載的場景
-  
+
     [Header("隨機場景列表")]
     [SerializeField] private List<GameSceneSO> randomScenes; // 可隨機選擇的場景列表
 
@@ -41,22 +41,22 @@ public class SceneLoader : MonoBehaviour,ISaveable
 
     private Vector3 positionToGo;//要傳送的位置
     private bool fadeScreen;//是否淡出屏幕
-    private  bool isLoading;//是否正在加載
+    private bool isLoading;//是否正在加載
 
     private void Start()
-    {    
-        loadEventSO.RaiseLoadRequestEvent(MuneScene, menuPosition, false);      
-    } 
+    {
+        loadEventSO.RaiseLoadRequestEvent(MuneScene, menuPosition, false);
+    }
     private void OnEnable()
     {
         loadEventSO.LoadRequestEvent += OnLoadRequestEvent;
         newGameEvent.OnEventRaised += OnNewGameStartEvent;
         backToMenuEvent.OnEventRaised += OnBackToMenuEvent;
         onAllEnemiesDefeated.OnEventRaised += OnOpenRandomCanvasEvent;//當場景中所有敵人被擊敗時通知UIManager
-        loadRandomSceneEvent.OnEventRaised += OnLoadRandomScene;
+        loadRandomSceneEvent.OnEventRaised += OnLoadRandomScene;//隨機挑戰場景加載事件
         ISaveable saveable = this;
         saveable.RegisterSaveData();
-    }  
+    }
 
     private void OnDisable()
     {
@@ -64,16 +64,15 @@ public class SceneLoader : MonoBehaviour,ISaveable
         newGameEvent.OnEventRaised -= OnNewGameStartEvent;
         backToMenuEvent.OnEventRaised -= OnBackToMenuEvent;
         onAllEnemiesDefeated.OnEventRaised -= OnOpenRandomCanvasEvent;
-        loadRandomSceneEvent.OnEventRaised -= OnLoadRandomScene;      
+        loadRandomSceneEvent.OnEventRaised -= OnLoadRandomScene;
         ISaveable saveable = this;
         saveable.UnRegisterSaveData();
     }
 
     private void OnNewGameStartEvent()//新遊戲事件時執行
     {
-        
-        sceneToLoad = firstLoadScene;    
-        loadEventSO.RaiseLoadRequestEvent(sceneToLoad, firstPosition, true);      
+        sceneToLoad = firstLoadScene;
+        loadEventSO.RaiseLoadRequestEvent(sceneToLoad, firstPosition, true);
     }
     private void OnBackToMenuEvent()//返回主菜單事件時執行
     {
@@ -101,8 +100,8 @@ public class SceneLoader : MonoBehaviour,ISaveable
         GameSceneSO randomScene = GetRandomScene();
         if (randomScene != null)
         {
-            loadEventSO.RaiseLoadRequestEvent(randomScene, firstPosition, true);
-           // OnLoadRequestEvent(randomScene, positionToGo, true);
+            sceneToLoad = randomScene;
+            loadEventSO.RaiseLoadRequestEvent(sceneToLoad, firstPosition,true);
         }
         else
         {
@@ -122,12 +121,12 @@ public class SceneLoader : MonoBehaviour,ISaveable
             return;
         isLoading = true;
         sceneToLoad = _locationToLaod;
-        positionToGo = _PosToGo;     
-        this.fadeScreen = fadeScreen;    
+        positionToGo = _PosToGo;
+        this.fadeScreen = fadeScreen;
 
         if (currentLoadScene != null)
         {
-            StartCoroutine(UnLoadPreviousScene());          
+            StartCoroutine(UnLoadPreviousScene());
         }
         else
         {
@@ -135,15 +134,14 @@ public class SceneLoader : MonoBehaviour,ISaveable
         }
     }
 
-    private IEnumerator UnLoadPreviousScene()
-    {      
+    private IEnumerator UnLoadPreviousScene()//卸載當前場景
+    {
+        yield return new WaitForSeconds(fadeTime);
         if (fadeScreen)
         {
-            //轉場
-            transitionEvent.TransitionIn();            
-        }    
-        yield return new WaitForSeconds(fadeTime);
-        unLoadSceneEvent.RaiseLoadRequestEvent(sceneToLoad,positionToGo,true);//廣播:調整血條顯示
+            transitionEvent.TransitionIn();//轉場
+        }
+        unLoadSceneEvent.RaiseLoadRequestEvent(sceneToLoad, positionToGo, true);//廣播:調整血條顯示
         yield return currentLoadScene.sceneReference.UnLoadScene();//卸載當前場景                                                               
         playerTrans.gameObject.SetActive(false); //關閉玩家人物
         LoadNewScene();//加載新場景
@@ -156,38 +154,33 @@ public class SceneLoader : MonoBehaviour,ISaveable
     /// <param name="_positionToGo">玩家要傳送到的位置。</param>
     /// <param name="fadeScreen">是否淡出屏幕。</param>
     private void LoadNewScene()//加載新場景
-    {       
-         var loadingOption = sceneToLoad.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);      
-         loadingOption.Completed += OnLoadComplete;        
+    {
+        var loadingOption = sceneToLoad.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
+        loadingOption.Completed += OnLoadComplete;
     }
 
     private void OnOpenRandomCanvasEvent()//當場景中所有敵人被擊敗時通知UIManager
     {
-        if(currentLoadScene.sceneType == SceneType.Location)
+        if (currentLoadScene.sceneType == SceneType.Location)
         {
             openRandomCanvaEvent.RaiseEvent();
         }
     }
-
-    ///<summary>
-    /// 加載完成後執行
-    ///</summary>
-    ///  <param name="_handle"></param>
-    private void OnLoadComplete(AsyncOperationHandle<SceneInstance> _handle)
-    {    
-        currentLoadScene = sceneToLoad;   
+    private void OnLoadComplete(AsyncOperationHandle<SceneInstance> _handle)// 加載完成後執行
+    {
+        currentLoadScene = sceneToLoad;
         playerTrans.position = positionToGo;
-        playerTrans.gameObject.SetActive(true);     
-        isLoading = false;
+        playerTrans.gameObject.SetActive(true);
+        isLoading = false;  
         if (currentLoadScene.sceneType == SceneType.Location)
         {
             saveDataEvent.RaiseEvent();//廣播:儲存加載遊戲事件
             afterSceneLoadedEvent.RaiseEvent();//廣播:已加載完成事件         
         }
-    } 
+    }
     public DataDefination GetDataID()
     {
-       return GetComponent<DataDefination>();
+        return GetComponent<DataDefination>();
     }
 
     /// <summary>
