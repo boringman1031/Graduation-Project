2b93f28d3fd0f41491dcd4719d291f6aa39ef429
Boringman1031 <ian.lin1031@gmail.com>
2025-03-22 21:55:10 +0800

    [update]重建必要關卡:聊天室

---
+++ Grduation_Game/Assets/Script/Utilities/TriggerMessage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMessage : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetTrigger("Message");
+        }
+    }
+}
