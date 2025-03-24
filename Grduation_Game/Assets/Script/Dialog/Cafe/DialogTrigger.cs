66ec00a3cc4327cc8c039b6d83bd67ceec9f1792
Boringman1031 <ian.lin1031@gmail.com>
2025-03-24 23:25:49 +0800

    [update]新增對話小遊戲:咖啡廳

---
+++ Grduation_Game/Assets/Script/Dialog/Cafe/DialogTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public VoidEventSO dialogEndEvent; // 對話結束事件
+    public GameObject QuestionGamePanel;
+    public string Key;
+    public bool isTalk = false;
+    private void OnEnable()
+    {
+        dialogEndEvent.OnEventRaised += OnDialogEnd;
+    }
+    private void OnDisable()
+    {
+        dialogEndEvent.OnEventRaised -= OnDialogEnd;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            DialogManager.Instance.StartDialog(Key);
+            isTalk = true;         
+        }
+    }
+
+    void OnDialogEnd()
+    {
+        if (isTalk)
+        {
+            QuestionGamePanel.SetActive(true);
+        }
+    }
+
+
+}
---
+++ Grduation_Game/Assets/Script/Dialog/QuestionManager.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour
+{
+    [Header("對話資料")]
+    public List<QuestionDataSO> dialogueList;
+    private List<QuestionDataSO> dialoguePool;
+    private QuestionDataSO currentQuestion;
+
+    [Header("UI組件")]
+    public List<Button> optionButtons;
+    public Text dialogueText; // 合併問題與回覆的文字框    
+    public Text affectionText;//好感度文字框
+    public GameObject nextButton;
+
+    private int affection = 0;
+
+    void Start()
+    {
+        dialoguePool = new List<QuestionDataSO>(dialogueList);
+        NextRandomQuestion();
+    }
+
+    void NextRandomQuestion()
+    {
+        if (dialoguePool.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        int index = Random.Range(0, dialoguePool.Count);
+        currentQuestion = dialoguePool[index];
+        dialoguePool.RemoveAt(index);
+
+        ShowDialogue(currentQuestion);
+    }
+
+    void ShowDialogue(QuestionDataSO data)
+    {
+        dialogueText.text = data.question;
+
+        for (int i = 0; i < optionButtons.Count; i++)
+        {
+            Text btnText = optionButtons[i].GetComponentInChildren<Text>();
+            btnText.text = data.options[i];
+
+            int tempIndex = i;
+            optionButtons[i].onClick.RemoveAllListeners();
+            optionButtons[i].onClick.AddListener(() => ChooseOption(tempIndex));
+        }
+
+        UpdateAffectionText();
+    }
+
+    void ChooseOption(int selectedIndex)
+    {
+        bool isCorrect = selectedIndex == currentQuestion.correctAnswerIndex;
+
+        if (isCorrect)
+        {
+            affection += currentQuestion.affectionChangeOnCorrect;
+            dialogueText.text = currentQuestion.correctReply;
+        }
+        else
+        {
+            affection += currentQuestion.affectionChangeOnWrong;
+            dialogueText.text = currentQuestion.wrongReply;
+        }
+
+        UpdateAffectionText();
+
+        foreach (var btn in optionButtons)
+            btn.interactable = false;
+
+        StartCoroutine(DelayNextDialogue(1.5f));
+    }
+
+    IEnumerator DelayNextDialogue(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach (var btn in optionButtons)
+            btn.interactable = true;
+
+        NextRandomQuestion();
+    }
+
+    void UpdateAffectionText()
+    {
+        affectionText.text = $"好感度：{affection}";
+    }
+
+    void EndDialogue()
+    {
+        if (affection >= 40)
+        {
+            dialogueText.text = "今天聊得很開心，下次再一起來吧！不過，如果有機會，我更想找個更浪漫的地方，只和你一起。";
+            nextButton.SetActive(true);
+        }
+        else
+        {
+            dialogueText.text = "...我還有點事，先走了...";
+            nextButton.SetActive(true);
+        }
+        foreach (var btn in optionButtons)
+        btn.gameObject.SetActive(false);
+    }
+}
