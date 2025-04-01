7f1c1b23bf63ab2eb7ef5044ae3d9ca0e1292c5a
喇叭鎖 <aszxer705266@gmail.com>
2025-04-01 13:59:23 +0800

    [update] 可以換裝了

--- Grduation_Game/Assets/Script/UI/Skill/PlayerCostumeChanger.cs
+++ Grduation_Game/Assets/Script/UI/Skill/PlayerCostumeChanger.cs
@@ -1,22 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
 public class PlayerCostumeChanger : MonoBehaviour
 {
-    public SpriteLibrary spriteLibrary;
+    [Header("Sprite Resolvers")]
+    public SpriteResolver head;
+    public SpriteResolver body;
+    public SpriteResolver leftArmUp;
+    public SpriteResolver leftArmDown;
+    public SpriteResolver rightArmUp;
+    public SpriteResolver rightArmDown;
+    public SpriteResolver leftLegUp;
+    public SpriteResolver leftLegDown;
+    public SpriteResolver rightLegUp;
+    public SpriteResolver rightLegDown;
+    public SpriteResolver left;
+    public SpriteResolver right;
 
     public void ChangeCostume(string label)
     {
-        if (spriteLibrary != null)
-        {
-            spriteLibrary.spriteLibraryAsset = Resources.Load<SpriteLibraryAsset>($"Costumes/{label}");
-            Debug.Log($"更換服裝為：{label}");
-        }
-        else
-        {
-            Debug.LogWarning("SpriteLibrary 未設定！");
-        }
+        head.SetCategoryAndLabel("Head", label);
+        body.SetCategoryAndLabel("Body", label);
+        leftArmUp.SetCategoryAndLabel("Left Arm UP", label);
+        leftArmDown.SetCategoryAndLabel("Left Arm Down", label);
+        rightArmUp.SetCategoryAndLabel("Right Arm UP", label);
+        rightArmDown.SetCategoryAndLabel("Right Arm Down", label);
+        leftLegUp.SetCategoryAndLabel("Left Leg UP", label);
+        leftLegDown.SetCategoryAndLabel("Left Leg Down", label);
+        rightLegUp.SetCategoryAndLabel("Right Leg UP", label);
+        rightLegDown.SetCategoryAndLabel("Right Leg Down", label);
+        left.SetCategoryAndLabel("Left", label);
+        right.SetCategoryAndLabel("Right", label);
+
+        Debug.Log("已切換裝扮為：" + label);
     }
 }
